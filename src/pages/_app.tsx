f6c306340f44a147e504f30b022d7ccc0771398d
Guilherme Eckert <guilherme-sonnemann@hotmail.com>
2021-04-21 10:01:51 -0700

    config json server, add styles to player and config _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,21 @@
-import { Header } from "../components/Header";
 import "../styles/global.scss";
+import { Header } from "../components/Header";
+
+
+import styles from "../styles/app.module.scss";
+import { Player } from "../components/Player";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-    <Header/>
-    <Component {...pageProps} />;
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />;
+      </main>
+      <Player/>
     </div>
-  )
+
+  );
 }
 
 export default MyApp;
